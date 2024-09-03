58f317e0a01bbff15c31318603f43066eec2fc1f
alexd <nothx@lol.gouv>
2024-09-03 14:40:40 -0400

    what the sigma?

--- draw.c
+++ draw.c
@@ -1,38 +1,88 @@
 #include <ncurses.h>
+#define height 20
+#define width 40
+const int start_x = 1;
+const int start_y = 1;
 
-int main() {
+int data[height][width] = {{0}};
+int n[height*width] = {{0}};
+int v[height*width] = {{0}};
+
+void drawSpot(WINDOW* win, int y, int x){
+    mvwprintw(win, y, x, "0");
+    mvwprintw(win, y-1, x, "0");
+    mvwprintw(win, y+1, x, "0");
+    mvwprintw(win, y, x+1, "0");
+    mvwprintw(win, y, x-1, "0");
+    data[y][x] = 1;
+    if(y-1 >=0)
+        data[y-1][x] = 1;
+    if(y+1 < height)
+        data[y+1][x] = 1;
+    if(x-1 >=0)
+        data[y][x-1] = 1;
+    if(x+1 < width)
+        data[y][x+1] = 1;
+}
+
+void printArr(){
+    for(int i=0;i<height;i++){
+        for(int j=0;j<width;j++){
+            mvprintw(20+i,20+j,"%i",data[i][j]); 
+        }
+    }
+}
+
+void display(){
     // Initialize ncurses
     initscr();            // Start ncurses mode
-    cbreak();             // Disable line buffering, pass all input directly
+    raw();             // Disable line buffering, pass all input directly
     noecho();             // Don't echo user input
     curs_set(FALSE);      // Hide the cursor
+    keypad(stdscr, TRUE);
+    mousemask(BUTTON1_PRESSED,NULL);
 
 
-    start_x = start_y = 20;
+    WINDOW* win = newwin(height, width, start_y, start_x);
 
-    /*mvprintw(0, 0, "test");*/
-	WINDOW *my_win;
-	int startx, starty, width, height;
-   
-	height = 3;
-	width = 10;
-	starty = (LINES - height) / 2;	/* Calculating for a center placement */
-	startx = (COLS - width) / 2;	/* of the window		*/
-	printw("Press F1 to exit");
-	refresh();
-	my_win = create_newwin(height, width, starty, startx);
-
-
-    wrefresh(subwin);
-
-    // Refresh the screen to show the message
     refresh();
+    wrefresh(win);
 
-    // Wait for user input
-    getch();
+    MEVENT event;
+    int ch;
+    int pressed = 0;
+    while(1){
+        box(win, 0, 0);
+        printArr();
+        wrefresh(win);
 
-    // End ncurses mode
+        ch = getch();
+        if(ch == 'q'){
+            break;
+        }
+        if(ch == 'd'){
+            werase(win);
+        }
+        if(ch == KEY_MOUSE){
+            getmouse(&event);
+            if(event.bstate & BUTTON1_PRESSED){
+                int x = event.x;
+                int y = event.y;
+                if(x>start_x && x<(width + start_x) 
+                    && y>start_y && y<(height+start_y)){
+                    drawSpot(win,y-start_y,x-start_x);
+                }
+            }
+        }
+        wrefresh(win);
+    }
+
     endwin();
+}
+
+
+int main() {
+    display();
 
     return 0;
 }
